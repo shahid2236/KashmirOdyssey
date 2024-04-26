55705eb76c39d24e34372130dd0ef47ed46ebd3f
Shahid Bhat <bhatshahid2236@gmail.com>
2024-04-27 02:00:14 +0530

    basic code uploaded

---
+++ script.js
@@ -0,0 +1,36 @@
+// Shery.imageEffect("#back", { style: 5, config: { "a": { "value": 2, "range": [0, 30] }, "b": { "value": 0.75, "range": [-1, 1] }, "zindex": { "value": -9996999, "range": [-9999999, 9999999] }, "aspect": { "value": 2.1852704927596696 }, "ignoreShapeAspect": { "value": true }, "shapePosition": { "value": { "x": 0, "y": 0 } }, "shapeScale": { "value": { "x": 0.5, "y": 0.5 } }, "shapeEdgeSoftness": { "value": 0, "range": [0, 0.5] }, "shapeRadius": { "value": 0, "range": [0, 2] }, "currentScroll": { "value": 0 }, "scrollLerp": { "value": 0.07 }, "gooey": { "value": true }, "infiniteGooey": { "value": true }, "growSize": { "value": 4, "range": [1, 15] }, "durationOut": { "value": 1, "range": [0.1, 5] }, "durationIn": { "value": 0.96, "range": [0.1, 5] }, "displaceAmount": { "value": 0.5 }, "masker": { "value": true }, "maskVal": { "value": 1, "range": [1, 5] }, "scrollType": { "value": 0 }, "geoVertex": { "range": [1, 64], "value": 1 }, "noEffectGooey": { "value": true }, "onMouse": { "value": 1 }, "noise_speed": { "value": 0.2, "range": [0, 10] }, "metaball": { "value": 0.14, "range": [0, 2], "_gsap": { "id": 3 } }, "discard_threshold": { "value": 0.5, "range": [0, 1] }, "antialias_threshold": { "value": 0, "range": [0, 0.1] }, "noise_height": { "value": 0.5, "range": [0, 2] }, "noise_scale": { "value": 10, "range": [0, 100] } }, gooey: true });
+
+
+var workingArea = document.querySelector("#workingarea");
+
+// var tl = gsap.timeline()
+// tl.from("#loader h3", {
+//     x: 200,
+//     opacity: 0,
+//     duration: 1,
+//     stagger: 0.3
+//   })
+//   tl.to("#loader", {
+//     opacity: 0,
+//     x: -40,
+//     stagger: 0.1,
+//     duration: 1
+//   })
+//   tl.to("#loader", {
+//     opacity: 0
+//   })
+
+// tl.from("#workingarea h1 span", {
+//     y: 100,
+//     opacity: 0,
+//     stagger: 0.5,
+//     duration: 0.6,
+//     delay: -0.5
+//   })
+
+//   tl.to("#loader", {
+//     display: "none"
+//   })
+
+
+
